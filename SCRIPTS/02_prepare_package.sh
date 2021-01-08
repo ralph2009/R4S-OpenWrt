ce1f61232c07718cf67058c7509f805dde69889d
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-01-08 19:17:19 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -329,6 +329,8 @@ rm -rf ./feeds/packages/utils/collectd
 svn co https://github.com/openwrt/packages/trunk/utils/collectd feeds/packages/utils/collectd
 svn co https://github.com/openwrt/packages/trunk/utils/usbutils feeds/packages/utils/usbutils
 ln -sf ../../../feeds/packages/utils/usbutils ./package/feeds/packages/usbutils
+svn co https://github.com/openwrt/packages/trunk/utils/hwdata feeds/packages/utils/hwdata
+ln -sf ../../../feeds/packages/utils/hwdata ./package/feeds/packages/hwdata
 #ipv6-helper
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/lean/ipv6-helper
 #IPSEC
